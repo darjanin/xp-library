6e4ccba5ad90c2eba3ecb7ca5356c9fae56fd740
Milan Darjanin <me@milandarjanin.com>
2016-03-19 19:33:46 +0100

    Initial project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # xp-library
+
+## Installation
+
+- You need to have locally installed npm and nodejs.
+- `npm install`.
+- You run the app using `npm server`.
+- Open in browser http://localhost:3000
+
+To learn what firebase is doing read documentation https://www.firebase.com/docs/web/guide/saving-data.html
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var Webpack = require('webpack')
+var path = require('path')
+var nodeModulesPath = path.resolve(__dirname, 'node_modules')
+var buildPath = path.resolve(__dirname, 'public', 'build')
+var mainPath = path.resolve(__dirname, 'app', 'main.js')
+
+var config = {
+  // Makes sure error in console map to the correct file and line number
+  devtool: 'eval',
+  entry: [
+    // For hot style updates
+    'webpack/hot/dev-server',
+
+    // The script refreshing the browser on none hot updates
+    'webpack-dev-server/client?http://localhost:8080',
+
+    // Our application
+    mainPath
+  ],
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [nodeModulesPath]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  plugins: [new Webpack.HotModuleReplacementPlugin()]
+}
+
+module.exports = config
