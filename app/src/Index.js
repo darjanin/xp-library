0108cfe664e94e4d1e6c9c4762708aee8819591c
OndrejRitomsky <o.ritomsky@gmail.com>
2016-03-31 19:20:51 +0200

    application, navigation, index components added

---
+++ app/src/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Index extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="content">
+            <h1> Welcome to Virtual Library </h1>
+        </div>
+    )
+    }
+}
---
+++ app/src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <nav className="navbar">
+                Navigation
+            </nav>
+        )
+    }
+}
